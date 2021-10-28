525a98d3a810d40cc7703681ffebf6cde151ed8f
Humberto Filho <hleitefilho@hotmail.com>
2021-10-28 20:47:37 -0300

    First library full version

---
+++ src/Berger.Global.Extensions/Json/JsonExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace Berger.Global.Extensions.Json
+{
+    public static class JsonExtensions
+    {
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static T FromJson<T>(this string json) => JsonSerializer.Deserialize<T>(json, _options);
+        public static string ToJson<T>(this T obj) => JsonSerializer.Serialize<T>(obj, _options);
+    }
+}
