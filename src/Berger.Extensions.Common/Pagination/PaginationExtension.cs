aede76f80d7b905e8b3cbaa59c27e0866b835f77
Humberto Filho <hleitefilho@hotmail.com>
2022-07-08 16:20:48 -0300

    Pagination models have been migrated to berger.marketplace.core

--- src/Berger.Extensions.Common/Pagination/PaginationExtension.cs
+++ src/Berger.Extensions.Common/Pagination/PaginationExtension.cs
@@ -1,6 +1,7 @@
-﻿using Berger.Extensions.Common.Auxiliar;
+﻿using Berger.Marketplace.Core.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace Berger.Extensions.Common.Pagination
 {
@@ -8,14 +9,40 @@ namespace Berger.Extensions.Common.Pagination
     {
         public static Pagination<T> Paginate<T>(this IQueryable<T> query, int page, int limit) where T : class
         {
-            var pagination = new Pagination<T>(query, page, limit);
-            
-            return pagination;
+            return query.MakePagination(page, limit);
         }
 
-        public static Pagination<T> Paginate<T>(this IList<T> query, int page, int limit) where T : class
+        public static Pagination<T> Paginate<T>(this IList<T> list, int page, int limit) where T : class
         {
-            var pagination = new Pagination<T>(query, page, limit);
+            return list.AsQueryable().MakePagination(page, limit);
+        }
+
+        private static Pagination<T> MakePagination<T>(this IQueryable<T> query, int page, int limit) where T : class
+        {
+            var pagination = new Pagination<T>();
+
+            var records = query.Count();
+            var pages = (double)records / limit;
+            var round = (int)Math.Ceiling(pages);
+            var skip = (page - 1) * limit;
+
+            var results = query.Count() <= limit ? query : query.Skip(skip).Take(limit);
+
+            var pageInfo = new PageInfo()
+            {
+                Current = page,
+                Previous = (page - 1 == 0) ? 1 : page - 1,
+                Next = (page + 1 == round + 1) ? round : page + 1,
+                HasNextPage = round >= page + 1,
+                HasPreviousPage = page > 1
+            };
+
+            pagination.TotalCount = records;
+            pagination.Limit = limit;
+            pagination.Pages = round;
+            pagination.Items = results.ToList();
+
+            pagination.PageInfo = pageInfo;
 
             return pagination;
         }
